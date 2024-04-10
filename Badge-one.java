2d4536e0d86ce8245394714223beaff7a0f32ff9
Israel Emmanuel <sriemmanuel787@outlook.com>
2024-04-10 23:40:51 +0000

    Extra error handling

--- Badge-one.java
+++ Badge-one.java
@@ -17,16 +17,16 @@ class Main {
         }
         open.close();
         System.out.println("template loaded");
-        
-        File registrations = new File("C:\\Users\\King\\Documents\\Java projects\\badge-creator\\IYC_24_Main_Registration_Responses_-_Region_3.csv");
+
+        File registrations = new File("IYC_24_Main_Registration_Responses_-_Region_3.csv");
         Scanner responses = new Scanner(registrations);
         responses.nextLine(); // i gotta advance this past the header
         ArrayList<String[]> resStore = new ArrayList<String[]>();
-        while(responses.hasNext()) {
+        while (responses.hasNext()) {
             resStore.add(Arrays.copyOfRange(responses.nextLine().split(","), 0, 11));
         }
 
-        File[] tickets = new File("C:\\Users\\King\\Documents\\Java projects\\badge-creator\\sample").listFiles();
+        File[] tickets = new File("sample").listFiles();
         for (int i = 0; i < tickets.length; i++) {
             Scanner tix = new Scanner(tickets[i]);
             ArrayList<String> ticketText = new ArrayList<String>();
@@ -42,26 +42,35 @@ class Main {
             String confirm = "";
             String barcode = "";
             String qrcode = "";
-            if (!ticketText.get(46).equals("		</text>")) {
-                confirm = ticketText.get(46).strip().substring(30);
-                barcode = ticketText.get(40).strip().split("\"")[5];
-                qrcode = "xlink:href=\"" + ticketText.get(7).strip().split("\"")[7] + "\" /><style";
-            } else {
-                confirm = ticketText.get(48).strip().substring(30);
-                barcode = ticketText.get(42).strip().split("\"")[5];
-                qrcode = "xlink:href=\"" + ticketText.get(7).strip().split("\"")[7] + "\" /><style";
+            try {
+                if (!ticketText.get(46).equals("		</text>")) {
+                    System.out.println(ticketText.get(41));
+                    confirm = ticketText.get(46).strip().substring(30);
+                    barcode = ticketText.get(40).strip().split("\"")[5];
+                    qrcode = "xlink:href=\"" + ticketText.get(7).strip().split("\"")[7] + "\" /><style";
+                } else {
+                    confirm = ticketText.get(48).strip().substring(30);
+                    barcode = ticketText.get(42).strip().split("\"")[5];
+                    qrcode = "xlink:href=\"" + ticketText.get(7).strip().split("\"")[7] + "\" /><style";
+                }
+            } catch (Exception e) {
+                System.out.println("failed 1");
             }
-            
 
             // Determine the age group, region, shirt size and name of the badge holder
-            for(int j = 0; j < resStore.size(); j++) {
-                if(resStore.get(j)[2].strip().equals(confirm)) {
-                    age = resStore.get(j)[10].strip();
-                    firstName = resStore.get(j)[3].strip();
-                    lastName = resStore.get(j)[4].strip();
-                    region = resStore.get(j)[0].strip().substring(0,2);
-                    size = resStore.get(j)[8].strip();
+            try {
+                for (int j = 0; j < resStore.size(); j++) {
+                    if (resStore.get(j)[2].strip().equals(confirm)) {
+                        age = resStore.get(j)[10].strip();
+                        firstName = resStore.get(j)[3].strip();
+                        lastName = resStore.get(j)[4].strip();
+                        region = resStore.get(j)[0].strip().substring(0, 2);
+                        size = resStore.get(j)[8].strip();
+                    }
                 }
+
+            } catch (Exception e) {
+                continue;
             }
 
             badge[423] = qrcode;
@@ -69,11 +78,13 @@ class Main {
             badge[420] = "id=\"tspan2652\">" + region + "</tspan></text>";
             badge[412] = "style=\"stroke-width:0\">" + size + "</tspan></text><text";
             badge[261] = "style=\"stroke-width:0\">" + firstName + "</tspan></text><g";
-            badge[250] = "style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-family:Montserrat;-inkscape-font-specification:'Montserrat Bold';stroke-width:0\">" + lastName + "</tspan></text><text";
+            badge[250] = "style=\"font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-family:Montserrat;-inkscape-font-specification:'Montserrat Bold';stroke-width:0\">"
+                    + lastName + "</tspan></text><text";
             badge[223] = "d=\"" + barcode + "\"";
-            badge[220] = "style=\"stroke-width:0\">" + confirm + "</tspan></text><path transform=\"scale(2.3794) translate(-126,333)\"";
+            badge[220] = "style=\"stroke-width:0\">" + confirm
+                    + "</tspan></text><path transform=\"scale(2.3794) translate(-126,333)\"";
 
-            File save = new File("C:\\Users\\King\\Documents\\Java projects\\badge-creator\\completed\\" + age + "\\badge" + (i + 1) + ".svg");
+            File save = new File("completed\\" + age + "\\badge" + (i + 1) + ".svg");
             FileWriter write = new FileWriter(save);
             // Age groups: 7-11, 12-18, 19-24, 25-34, 35-44, 45+
             for (int k = 0; k < badge.length; k++) {
